d65d557bfdcd812659af87cabe78d017d30b7443
Adarsh Kumar <adarshkumar6520@gmail.com>
2026-07-07 14:18:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app=express()
 app.post("/api/stripe/webhook",express.raw({type:"application/json"}),stripeWebhook)
 const port = process.env.PORT || 5000;
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://srijanai-1.onrender.com",
     credentials:true
 }))
 app.use(express.json())
